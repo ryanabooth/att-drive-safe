ab4c8601add63e3b540801df8f2947c89178359d
Ryan Booth <ryanbooth77@gmail.com>
2015-01-03 22:09:37 -0800

    init

---
+++ firstPage/controllers/FirstPageCtrl.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+angular.module('app')
+    .controller('FirstPageCtrl', ["$scope", function ($scope) {
+
+        $rootScope.$watch('decInstance.isOnline', function(value){
+            $scope.isDecOnline = value;
+        });
+
+        var deregisterIdentificationWatch = $rootScope.$watch('identification', function (value, oldvalue) {
+            if (!value || angular.equals(value, oldvalue)) return;
+            console.info('Change detected on the identification namespace: ', value);
+            $scope.identification = $rootScope.identification;
+        });
+
+
+        $scope.drivers = [
+          { text: 'Ryan', desc: 'Booth', href: '', selected: true },
+          { text: 'Lucas', desc: '', href: '', selected: false },
+          { text: 'Michael', desc: '', href: '', selected: false },
+          { text: 'Badge', desc: '', href: '', selected: false }
+        ];
+
+        $scope.$on('$destroy', function () {
+            //stop watching when scope is destroyed
+            if (deregisterIdentificationWatch) deregisterIdentificationWatch();
+        });
+
+    }]);
